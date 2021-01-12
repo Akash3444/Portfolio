fc451f8f069a212479acd0e172499c6eb84e9fcc
Akash3444 <akashmoradiya3444@gmail.com>
2021-01-12 20:50:22 +0530

    portfolio sections created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Services from "./components/Services";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import "./style.css";
+import "./App.css";
 
 export default function App() {
   return (
